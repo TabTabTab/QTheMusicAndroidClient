117e924ebd35f03f3cc7f57932a36211b3b6edef
Denhi Huynh <Denhi_@hotmail.com>
2015-05-19 16:21:39 +0200

    Added home screen

---
+++ app/src/main/java/com/tabtabtab/johan/denounce/HomeScreenFragment.java
@@ -0,0 +1,77 @@
+package com.tabtabtab.johan.denounce;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class HomeScreenFragment extends Fragment implements View.OnClickListener {
+    private Activity activity;
+    private Vibrator vibration;
+
+    public static HomeScreenFragment newInstance() {
+        HomeScreenFragment fragment = new HomeScreenFragment();
+        return fragment;
+    }
+
+    public HomeScreenFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_home_screen, container, false);
+        Button client = (Button) rootview.findViewById(R.id.client_btn);
+        Button host = (Button) rootview.findViewById(R.id.host_btn);
+        client.setOnClickListener(this);
+        host.setOnClickListener(this);
+
+        // Inflate the layout for this fragment
+        return rootview;
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.activity = activity;
+        vibration = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.client_btn:
+                vibration.vibrate(50);
+                getFragmentManager().beginTransaction()
+                        .add(R.id.container, new SubscriberFragment())
+                        .commit();
+                break;
+            case R.id.host_btn:
+                vibration.vibrate(50);
+                Toast.makeText(activity, "To be implemented",
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+}
--- app/src/main/java/com/tabtabtab/johan/denounce/MainActivity.java
+++ app/src/main/java/com/tabtabtab/johan/denounce/MainActivity.java
@@ -13,6 +13,7 @@ import monitor.ClientMonitor;
 public class MainActivity extends FragmentActivity {
     private ClientMonitor monitor;
     private SubscriberFragment subFragment;
+    private HomeScreenFragment homeScreenFragment;
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,37 +21,38 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             subFragment = new SubscriberFragment();
+            homeScreenFragment = new HomeScreenFragment();
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, subFragment)
+                    .add(R.id.container, homeScreenFragment)
                     .commit();
         }
     }
 
-    @Override
-    public void onBackPressed() {
-
-       final android.support.v4.app.FragmentManager fragman = subFragment.getFragMan();
-        int count = fragman.getBackStackEntryCount();
-
-        if (count == 0) {
-            super.onBackPressed();
-        } else {
-            new AlertDialog.Builder(this)
-                    .setTitle("Disconnect?")
-                    .setMessage("Are you sure you want to disconnect to host?")
-                    .setNegativeButton(android.R.string.no, null)
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-
-                        public void onClick(DialogInterface arg0, int arg1) {
-                            ClientMonitor monitor = subFragment.getClientMonitor();
-                            if (monitor != null) {
-                                monitor.closeConnection();
-                                fragman.popBackStack();
-                            }
-                        }
-                    }).create().show();
-        }
-
-    }
+//    @Override
+//    public void onBackPressed() {
+//
+//       final android.support.v4.app.FragmentManager fragman = subFragment.getFragMan();
+//        int count = fragman.getBackStackEntryCount();
+//
+//        if (count == 0) {
+//            super.onBackPressed();
+//        } else {
+//            new AlertDialog.Builder(this)
+//                    .setTitle("Disconnect?")
+//                    .setMessage("Are you sure you want to disconnect to host?")
+//                    .setNegativeButton(android.R.string.no, null)
+//                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+//
+//                        public void onClick(DialogInterface arg0, int arg1) {
+//                            ClientMonitor monitor = subFragment.getClientMonitor();
+//                            if (monitor != null) {
+//                                monitor.closeConnection();
+//                                fragman.popBackStack();
+//                            }
+//                        }
+//                    }).create().show();
+//        }
+//
+//    }
 
 }
